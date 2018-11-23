eb3188b36d99a609f69e00a697f24c21ce92dcac
Thiago Vilella <41455040+TVilella@users.noreply.github.com>
2018-11-22 22:11:51 -0200

    included library files

---
+++ src/PiscaLed.h
@@ -0,0 +1,19 @@
+#ifndef PiscaLed_h
+#define PiscaLed_h
+
+#include "Arduino.h"
+
+class PiscaLed {
+  public:
+    PiscaLed(byte pinLED, int tempoPisca = 1000);
+    void play(); 
+    void stop();
+    void loop();
+  private:
+    unsigned long delayPisca;
+    bool estadoPisca;
+    byte pino;
+    int  tempo;
+};
+
+#endif
